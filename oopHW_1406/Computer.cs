95fc5452226fb45600c0485095eb0def78323386
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-06-15 13:43:37 +0300

    Add files via upload

---
+++ oopHW_1406/Computer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopHW_1406
+{
+    [DebuggerDisplay("{model} {(price * 3.67)}$")]
+    public class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn;
+
+        public void tellMeThePrice()
+        {
+            Console.WriteLine($"price {model}: {price}");
+        }
+
+        public void tellMeTheScreenSize()
+        {
+            Console.WriteLine($"{model} screen size: {screenSize}");
+        }
+
+        public void turnOn()
+        {
+            Console.WriteLine($"The divice is on...");
+            isTurnOn = true;
+        }
+
+        public void turnOff()
+        {
+            Console.WriteLine($"The divice is off...");
+            isTurnOn = false;
+        }
+
+        public void addProcessor()
+        {
+            Console.WriteLine("another processor");
+            numberOfProcessors++;
+        }
+
+        public override string ToString()
+        {
+            return $"[Computer]: model: {model} Price: {price} Processor number: {numberOfProcessors} Screen Size: {screenSize} isTurnOn: {isTurnOn} ";
+        }
+    }
+}
---
+++ oopHW_1406/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopHW_1406
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer asus = new Computer()
+            {
+                model = "ROG",
+                price = 6459,
+                numberOfProcessors = 1,
+                screenSize = 15.7f,
+                isTurnOn = true
+            };
+
+            Computer lenovo = new Computer()
+            {
+                model = "Z51",
+                price = 8759,
+                numberOfProcessors = 0,
+                screenSize = 17.8f,
+                isTurnOn = false
+            };
+
+            Computer corsair = new Computer()
+            {
+                model = "One i160",
+                price = 3562,
+                numberOfProcessors = 2,
+                screenSize = 15.7f,
+                isTurnOn = false
+            };
+
+            Console.WriteLine(asus);
+
+            asus.tellMeThePrice();
+            asus.tellMeTheScreenSize();
+            asus.turnOn();
+            asus.addProcessor();
+
+        }
+    }
+}
